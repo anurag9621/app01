3e84e70f3e6f65012f7a7412fa219e3a31067bac
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-19 16:01:50 +0530

    ok

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.scrollview_example)
     }
 
     override fun onStart() {
