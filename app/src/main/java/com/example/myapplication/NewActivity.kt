2029a176593f3a5904ebda8c1cd880e7e877b6b7
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-19 21:49:13 +0530

    ok

--- app/src/main/java/com/example/myapplication/NewActivity.kt
+++ app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -10,26 +10,30 @@ import android.widget.Toast
 
 class NewActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
-        Toast.makeText(this@NewActivity,"we clicked on the button to see this Toast",Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            this@NewActivity,
+            "we clicked on the button to see this Toast",
+            Toast.LENGTH_LONG
+        ).show()
     }
-    lateinit var etMobileNumber:EditText
-    lateinit var etPassword:EditText
-    lateinit var btnLogin:Button
-    lateinit var txtForgotPassword:TextView
-    lateinit var txtRegister:TextView
 
+    lateinit var etMobileNumber: EditText
+    lateinit var etPassword: EditText
+    lateinit var btnLogin: Button
+    lateinit var txtForgotPassword: TextView
+    lateinit var txtRegister: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
-        title="Log In"
+        title = "Log In"
 
-        etMobileNumber=findViewById(R.id.etMobileNumber)
-        etPassword=findViewById(R.id.etPassword)
-        txtForgotPassword=findViewById(R.id.txtForgotPassword)
-        txtRegister=findViewById(R.id.txtRegister)
-        btnLogin=findViewById(R.id.btnLogin)
+        etMobileNumber = findViewById(R.id.etMobileNumber)
+        etPassword = findViewById(R.id.etPassword)
+        txtForgotPassword = findViewById(R.id.txtForgotPassword)
+        txtRegister = findViewById(R.id.txtRegister)
+        btnLogin = findViewById(R.id.btnLogin)
 
         btnLogin.setOnClickListener(this)
 
