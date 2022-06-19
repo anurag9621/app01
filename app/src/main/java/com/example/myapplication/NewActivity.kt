7ccc37136cbaaaadfdd522c802032c34b9a6c669
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-19 19:03:45 +0530

    ok

---
+++ app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+
+        title="Log In"
+    }
+}
