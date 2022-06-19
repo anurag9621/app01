11cfa7d9ae6e32aa477f45bb2ea27e7017f6b72f
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-19 19:50:27 +0530

    ok

--- app/src/main/java/com/example/myapplication/NewActivity.kt
+++ app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -2,12 +2,38 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
-class NewActivity : AppCompatActivity() {
+class NewActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        Toast.makeText(this@NewActivity,"we clicked on the button to see this Toast",Toast.LENGTH_LONG).show()
+    }
+    lateinit var etMobileNumber:EditText
+    lateinit var etPassword:EditText
+    lateinit var btnLogin:Button
+    lateinit var txtForgotPassword:TextView
+    lateinit var txtRegister:TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
-
         title="Log In"
+
+        etMobileNumber=findViewById(R.id.etMobileNumber)
+        etPassword=findViewById(R.id.etPassword)
+        txtForgotPassword=findViewById(R.id.txtForgotPassword)
+        txtRegister=findViewById(R.id.txtRegister)
+        btnLogin=findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener(this)
+
     }
+
+
 }
